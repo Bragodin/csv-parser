b0c1381009f34f6108669bfb76d2f64a0fa50fe8
Drozd Egor <drozdegor.sr@gmail.com>
2019-03-21 03:10:52 +0300

    parser validatino and datebase

--- parser.ts
+++ parser.ts
@@ -1,12 +1,13 @@
+import { validator }  from './valid';
+import { dataBase } from './dataBase';
 const csv = require('csv-parser');
 const fs = require('fs');
 const results = [];
- 
+
 fs.createReadStream('data.csv')
-  .pipe(csv())
+  .pipe(csv({separator: ';'}))
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    console.log(results);
-
-    
+    console.log(validator(results));
+    dataBase(results);
   });
